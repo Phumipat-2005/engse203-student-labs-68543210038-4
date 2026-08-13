58be02061114a8d6a008f645dc99cef78b7ea163
Phumipat-2005 <215790260+Phumipat-2005@users.noreply.github.com>
2026-08-13 17:44:18 +0700

    week05-lab05-5A-CP02

--- labs/week-05/starter/src/App.jsx
+++ labs/week-05/starter/src/App.jsx
@@ -1,13 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import AppLayout from './pages/AppLayout.jsx';
+import AboutPage from './pages/AboutPage.jsx';
 import DashboardPage from './pages/DashboardPage.jsx';
+import NewRequestPage from './pages/NewRequestPage.jsx';
+import NotFoundPage from './pages/NotFoundPage.jsx';
+import RequestDetailPage from './pages/RequestDetailPage.jsx';
 
 function App() {
   return (
-    <AppLayout>
-      <DashboardPage />
-    </AppLayout>
+    <Routes>
+      <Route element={<AppLayout />}>
+        <Route index element={<DashboardPage />} />
+        <Route element={<AboutPage />} path="about" />
+        <Route element={<NewRequestPage />} path="request/new" />
+        <Route element={<RequestDetailPage />} path="request/:requestId" />
+        <Route element={<NotFoundPage />} path="*" />
+      </Route>
+    </Routes>
   );
-  // TODO 5A-CP02: เปลี่ยนเป็น <Routes> ที่มี AppLayout เป็นกรอบ
 }
 
 export default App;
